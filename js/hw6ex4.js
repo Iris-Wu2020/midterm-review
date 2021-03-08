ad2f1ef4fbac51fa0e5b89b0bc86345a37ecb7f5
IrisWu <xianwu_iris@126.com>
2021-03-08 22:28:56 +0800

    change

--- js/hw6ex4.js
+++ js/hw6ex4.js
@@ -6,17 +6,34 @@ console.log("Wu's Output from hw6 Exercise 4");
 
 
 /* 
-<TABLE border = 4 width = 50 > 
+<TABLE> 
         <TR>  
             <TD> 1 </TD>   
         </TR> 
 </Table> 
  */
 
+// create the table
+const tableNode = document.createElement("table");
+const divNode = document.getElementById("output");
+
+
+for (let i = 1; i < 13; i++) {
+    const trNode = document.createElement("tr");
+    const tdNode = document.createElement("td");
+    const dataText = document.createTextNode(i);
+    trNode.appendChild(tdNode).appendChild(dataText);
+    tableNode.appendChild(trNode);
+    divNode.appendChild(tableNode);
+
+};
+
+tableNode.setAttribute("border", "2");
+tableNode.setAttribute("width", "30");
+
+// filter the table
 const elementNames = document.getElementsByTagName('tr');
 const elementList = Array.from(elementNames).filter(element => Number(element.textContent) % 4 === 0);
 Array.from(elementList).map(element => element.classList.add('fourDivi'));
-// const elementList = Array.from(elementNames).map(element => Number(element.textContent));
+
 console.log(elementList);
-// const titleElement = document.querySelectorAll("td");
-// console.log(titleElement);
