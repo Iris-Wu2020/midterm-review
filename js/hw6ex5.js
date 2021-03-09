b44b7413f3435ccaceb5045665ee8f23af914299
IrisWu <xianwu_iris@126.com>
2021-03-09 11:26:59 +0800

    change

--- js/hw6ex5.js
+++ js/hw6ex5.js
@@ -4,19 +4,19 @@ Exercise 5 JavaScript code
 console.log("Wu's Output from hw6 Exercise 5");
 
 //create a tag <a>
-const linkElement = document.createElement("a");
-linkElement.href = "https://www.csulb.edu/college-of-business";
-linkElement.textContent = "College of Business";
-console.log(linkElement);
+const aNode = document.createElement("a");
+aNode.href = "https://www.csulb.edu/college-of-business";
+aNode.textContent = "College of Business";
+console.log(aNode);
 
 //create another tag <li>, and then add tag <a> to tag <li>
-const linkElement2 = document.createElement("li");
-linkElement2.appendChild(linkElement);
-console.log(linkElement2);
+const liNode = document.createElement("li");
+liNode.appendChild(aNode);
+console.log(liNode);
 
 const firstLink = document.querySelectorAll("li");
 console.log(firstLink[1]); //This is the second link
 
-document.getElementsByTagName("ul")[0].insertBefore(linkElement2, firstLink[1]);
+document.getElementsByTagName("ul")[0].insertBefore(liNode, firstLink[1]);
 
 
